8db55d0573af2076b4b2ceac1392bee07e086e66
RENMarto <153503493+RENMarto@users.noreply.github.com>
2026-06-08 17:08:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "Hello World,""\n\n";
-echo "this is on Webserver " . php_uname('n') . ""\n\n" and this is running on OS" . php_uname('s');
+echo "Hello World,\n\n";
+echo "this is on Webserver " . php_uname('n') . "and this is running on OS" . php_uname('s');
 ?>
